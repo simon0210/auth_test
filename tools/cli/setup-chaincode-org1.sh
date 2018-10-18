9f1142115b4a124f0aaa040c33a900e8be82b404
김승광 <seoungkwang.kim@kt.com>
2018-10-18 11:42:22 +0900

    KT 환경 설정.

--- tools/cli/setup-chaincode-org1.sh
+++ tools/cli/setup-chaincode-org1.sh
@@ -2,15 +2,15 @@
 
 echo 'Install ChainCode'
 
-CORE_PEER_ADDRESS=peer0.org1.skinfosec.com:7051 peer chaincode install -n Certificate -v 1.1 -p github.com/hyperledger/fabric/examples/chaincode/go/certificate_cc
-CORE_PEER_ADDRESS=peer1.org1.skinfosec.com:7051 peer chaincode install -n Certificate -v 1.1 -p github.com/hyperledger/fabric/examples/chaincode/go/certificate_cc
+CORE_PEER_ADDRESS=peer0.org1.kt.com:7051 peer chaincode install -n Certificate -v 1.1 -p github.com/hyperledger/fabric/examples/chaincode/go/certificate_cc
+CORE_PEER_ADDRESS=peer1.org1.kt.com:7051 peer chaincode install -n Certificate -v 1.1 -p github.com/hyperledger/fabric/examples/chaincode/go/certificate_cc
 
 echo 'Done'
 
 echo 'Instantiate ChainCode'
 
-CORE_PEER_ADDRESS=peer0.org1.skinfosec.com:7051 peer chaincode instantiate -o orderer.skinfosec.com:7050 --tls $CORE_PEER_TLS_ENABLED \
---cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/skinfosec.com/orderers/orderer.skinfosec.com/msp/tlscacerts/tlsca.skinfosec.com-cert.pem \
+CORE_PEER_ADDRESS=peer0.org1.kt.com:7051 peer chaincode instantiate -o orderer.kt.com:7050 --tls $CORE_PEER_TLS_ENABLED \
+--cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/kt.com/orderers/orderer.kt.com/msp/tlscacerts/tlsca.kt.com-cert.pem \
 -C channel1 -n Certificate -v 1.1 -c '{"Args":["init","a","100","b","200"]}' -P "OR   ('Org1MSP.member','Org2MSP.member')"
 
 echo 'Done'
